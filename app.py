e2c21b7f820446c16985e635d032f4a6d4b1c5c3
Tue Topholm <tt@gajin.dk>
2024-10-07 19:54:28 +0200

    small fix

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def scrape():
 
     try:
         browser.get("https://www.minvandforsyning.dk/LoginIntermediate")
-        wait_for_element(browser, '//div[3]/button', 10)
-        browser.find_element(By.XPATH, '//div[3]/button').click()
-        browser.find_element(By.XPATH, "//div[@id='LoginIntermediaryMudPaper']/div/div/button").click()
+        wait_for_element(browser, '/html/body/app/body/div[2]/div/div/div[3]/button', 10)
+        browser.find_element(By.XPATH, '//html/body/app/body/div[2]/div/div/div[3]/button').click()
+        browser.find_element(By.XPATH, "//*[@id="LoginIntermediaryMudPaper"]/div[1]/div[1]/button").click()
         wait_for_element(browser, "//input[@type='text']", 10)
         browser.find_element(By.XPATH, "//input[@type='text']").send_keys(mvf_username)
         browser.find_element(By.XPATH, "//input[@type='password']").send_keys(mvf_password)
